396ff1b3d752c22186bdcf41142db85c9ec4b856
Dev <mbradle6@unca.edu>
2023-02-22 14:13:50 -0500

    working on showing posts

--- photo-app/js/main.js
+++ photo-app/js/main.js
@@ -10,21 +10,46 @@ const showStories = async (token) => {
         }
     })
     const data = await response.json();
-    console.log('Stories:', data);
+    const HTMLChunk = data.map(storiesToHTML).join('');
+    document.querySelector('.accounts').innerHTML = HTMLChunk;
 }
 
 const showPosts = async (token) => {
-    console.log('code to show posts');
+    const endpoint = `${rootURL}/api/post`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    const HTMLSnippit = data.map(postsToHTML).join('');
+    document.querySelector('.feed').innerHTML = HTMLSnippit;
+}
+const postsToHTML = post =>{
+    return ` <div class="posttitle">
+    <p>${post.user.username}</p>
+    <p>...</p>
+ </div>
+ <img src="${post.user.image_url}" width="100%"/>`
 }
 
+const storiesToHTML = story =>{
+return  `<div class="story">
+    <img class="profilepictures" src="${story.user.thumb_url}" width="50"/>
+    <p>${story.user.username}</P>
+</div>`
 
+}
 const initPage = async () => {
     // first log in (we will build on this after Spring Break):
     const token = await getAccessToken(rootURL, 'webdev', 'password');
-
+    console.log(token);
     // then use the access token provided to access data on the user's behalf
     showStories(token);
     showPosts(token);
-}
 
+    //query for users profile and suggested people
+}
+// Kicks off the website
 initPage();
